82290557e3f120b5e5d81b2fc0ae58dbfa2baadc
Julio <jpatinoparra@frba.utn.edu.ar>
2025-04-28 12:14:36 -0300

    Agrego datas: Poder y Personaje

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,19 @@ import Text.Show.Functions ()
 
 doble :: Int -> Int
 doble x = x * 2
+
+data Personaje = UnPersonaje {
+  nombre :: String,
+  poderBasico :: Poder,
+  superPoder :: Poder,
+  superActivo :: Bool,
+  vida :: Int
+} deriving Show
+
+data Poder
+  = BolaEspinosa
+  | LluviaDeTuercasSanadora
+  | LluviaDeTuercasDanina
+  | GranadaDeEspinas Int  
+  | TorretaCurativa
+  deriving Show
